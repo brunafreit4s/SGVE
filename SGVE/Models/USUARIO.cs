35bf71059746f530d9694911897d6042c8a94169
brunafreit4s <bubufreitas@hotmail.com>
2022-03-09 18:42:36 -0300

    Inciando métodos de consulta

---
+++ SGVE/Models/USUARIO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGVE.Models
+{
+    public class USUARIO
+    {
+        public string LOGIN { get; set; }
+        public string SENHA { get; set; }
+    }
+}
