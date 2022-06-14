0c9d43ad8645362ed70b36f228e19abcba347e5e
brunafreit4s <bubufreitas@hotmail.com>
2022-06-14 18:52:07 -0300

    Adicionado gráfico pizza para quantidade de produtos em estoque

---
+++ SGVE/Models/Produtos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGVE.Models
+{
+    public class Produtos
+    {
+        public string COD_BARRAS { get; set; }
+        public string NOME_PROD { get; set; }
+        public string MARCA_PROD { get; set; }
+        public double PRECO_PROD { get; set; }
+        public double CUSTO_PROD { get; set; }
+        public string DATA_CAD_PROD { get; set; }
+        public int QUANTIDADE_PROD { get; set; }
+        public string DESC_PROD { get; set; }
+    }
+}
