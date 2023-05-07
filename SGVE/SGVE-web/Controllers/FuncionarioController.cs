df26f1d28eaac4213a882b5d5207039f1a405c0b
brunafreit4s <bubufreitas@hotmail.com>
2023-05-06 22:13:12 -0300

    Ajustado máscaras campos do formulário de funcionário

--- SGVE/SGVE-web/Controllers/FuncionarioController.cs
+++ SGVE/SGVE-web/Controllers/FuncionarioController.cs
@@ -21,6 +21,13 @@ namespace SGVE_web.Controllers
         {
             var accessToken = await HttpContext.GetTokenAsync("access_token");  /* Retorna access token para utilizar no swagger */
             var funcionarios = await _funcionarioService.FindAllFuncionarios(accessToken);
+
+            foreach (var item in funcionarios)
+            {
+                item.Cpf = Convert.ToUInt64(item.Cpf).ToString(@"000\.000\.000\-00");
+                item.Rg = Convert.ToUInt64(item.Rg).ToString(@"00000000\-00");
+            }
+
             return View(funcionarios);
         }
 
@@ -35,7 +42,7 @@ namespace SGVE_web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var accessToken = await HttpContext.GetTokenAsync("access_token");  /* Retorna access token para utilizar no swagger */
+                var accessToken = await HttpContext.GetTokenAsync("access_token");  
                 var response = await _funcionarioService.CreateFuncionario(model, accessToken);
                 if (response != null) return RedirectToAction(nameof(Index));
             }
@@ -45,9 +52,17 @@ namespace SGVE_web.Controllers
 
         public async Task<ActionResult> Update(int id)
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");  /* Retorna access token para utilizar no swagger */
+            var accessToken = await HttpContext.GetTokenAsync("access_token"); 
             var funcionario = await _funcionarioService.FindByIdFuncionario(id, accessToken);
-            if (funcionario != null) return View(funcionario);
+
+            if (funcionario != null)
+            {
+                funcionario.Cpf = Convert.ToUInt64(funcionario.Cpf).ToString(@"000\.000\.000\-00");
+                funcionario.Rg = Convert.ToUInt64(funcionario.Rg).ToString(@"000000000\-00");
+                funcionario.Celular = Convert.ToUInt64(funcionario.DDD_Celular + funcionario.Celular).ToString(@"(000) 00000-0000");
+                return View(funcionario);
+            }
+
             return NotFound();
         }
 
@@ -57,7 +72,7 @@ namespace SGVE_web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var accessToken = await HttpContext.GetTokenAsync("access_token");  /* Retorna access token para utilizar no swagger */
+                var accessToken = await HttpContext.GetTokenAsync("access_token"); 
                 var response = await _funcionarioService.UpdateFuncionario(model, accessToken);
                 if (response != null) return RedirectToAction(nameof(Index));
             }
@@ -68,9 +83,17 @@ namespace SGVE_web.Controllers
         [Authorize]
         public async Task<ActionResult> Delete(int id)
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");  /* Retorna access token para utilizar no swagger */
+            var accessToken = await HttpContext.GetTokenAsync("access_token");  
             var funcionario = await _funcionarioService.FindByIdFuncionario(id, accessToken);
-            if (funcionario != null) return View(funcionario);
+
+            if (funcionario != null)
+            {
+                funcionario.Cpf = Convert.ToUInt64(funcionario.Cpf).ToString(@"000\.000\.000\-00");
+                funcionario.Rg = Convert.ToUInt64(funcionario.Rg).ToString(@"000000000\-00");
+                funcionario.Celular = Convert.ToUInt64(funcionario.DDD_Celular + funcionario.Celular).ToString(@"(000) 00000-0000");
+                return View(funcionario);
+            }
+
             return NotFound();
         }
 
@@ -78,7 +101,7 @@ namespace SGVE_web.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<ActionResult> Delete(FuncionarioViewModel model)
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");  /* Retorna access token para utilizar no swagger */
+            var accessToken = await HttpContext.GetTokenAsync("access_token"); 
             var response = await _funcionarioService.DeleteFuncionario(model.Id, accessToken);
             if (response) return RedirectToAction(nameof(Index));
 
