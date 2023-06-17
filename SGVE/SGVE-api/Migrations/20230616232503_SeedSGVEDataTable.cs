64c8e6a5f5223a042861c3e294f3d6a8c5572d10
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-06-16 21:52:22 -0300

    Ajustes base

---
+++ SGVE/SGVE-api/Migrations/20230616232503_SeedSGVEDataTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGVEapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSGVEDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TB_PRODUTO",
+                keyColumn: "ID_PRODUTO",
+                keyValue: 2L,
+                columns: new[] { "DT_CADASTRO", "FK_ID_FORNECEDOR", "FK_ID_TIPO" },
+                values: new object[] { new DateTime(2023, 6, 16, 20, 25, 3, 500, DateTimeKind.Local).AddTicks(5885), 2, 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TB_PRODUTO",
+                keyColumn: "ID_PRODUTO",
+                keyValue: 2L,
+                columns: new[] { "DT_CADASTRO", "FK_ID_FORNECEDOR", "FK_ID_TIPO" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, 0 });
+        }
+    }
+}
---
+++ SGVE/SGVE.IdentityServer/Migrations/20230616232544_SeedSGVEDataTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGVE.IdentityServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSGVEDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
